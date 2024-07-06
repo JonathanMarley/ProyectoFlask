9ea339350704e2cf0f5ef4dc08947308e9700a91
jonat <jonathanmarley2536@gmail.com>
2024-07-06 12:23:17 -0600

    Finalizacion

--- app.py
+++ app.py
@@ -1,73 +1,88 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
+# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'contactospersonas'
-mysql = MySQL(app)
 
-#settings
+# Configuración de la base de datos MySQL
+app.config['MYSQL_HOST'] = 'localhost'  # Dirección del servidor de la base de datos
+app.config['MYSQL_USER'] = 'root'       # Usuario de la base de datos
+app.config['MYSQL_PASSWORD'] = ''       # Contraseña del usuario de la base de datos
+app.config['MYSQL_DB'] = 'contactospersonas'  # Nombre de la base de datos
+mysql = MySQL(app)  # Inicializa la extensión Flask-MySQLdb con la configuración de la app
+
+# Configuración de la clave secreta para la aplicación Flask
 app.secret_key = 'mysecretkey'
 
-
-
+# Ruta principal de la aplicación, método GET por defecto
 @app.route('/')
 def Index(): 
- cursor = mysql.connection.cursor()
- cursor.execute("SELECT * FROM contactos")
- data = cursor.fetchall()
- return render_template('index.html', contacts = data)
+    cursor = mysql.connection.cursor()  # Crear un cursor para interactuar con la base de datos
+    cursor.execute("SELECT * FROM contactos")  # Ejecutar una consulta SQL para obtener todos los contactos
+    data = cursor.fetchall()  # Obtener todos los resultados de la consulta
+    return render_template('index.html', contacts=data)  # Renderizar la plantilla index.html con los datos obtenidos
 
+# Ruta para agregar un nuevo contacto, método POST
 @app.route('/agregarContacto', methods=['POST'])
 def agregarContacto():
- if request.method == 'POST':
-   nombre = request.form['nombre']
-   apellido = request.form['apellido']
-   telefono = request.form['telefono']
-   correo = request.form['correo']
-   cursor = mysql.connection.cursor()
-   cursor.execute("INSERT INTO contactos (nombre, apellido, telefono, correo) VALUES (%s, %s, %s, %s)", (nombre, apellido, telefono, correo))
-   mysql.connection.commit()
-   flash('Contacto agregado exitosamente')
-   return redirect(url_for('Index'))
- 
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        telefono = request.form['telefono']
+        correo = request.form['correo']
+        
+        # Crear un cursor para interactuar con la base de datos
+        cursor = mysql.connection.cursor()
+        # Ejecutar una consulta SQL para insertar el nuevo contacto
+        cursor.execute("INSERT INTO contactos (nombre, apellido, telefono, correo) VALUES (%s, %s, %s, %s)", 
+                       (nombre, apellido, telefono, correo))
+        mysql.connection.commit()  # Confirmar los cambios en la base de datos
+        flash('Contacto agregado exitosamente')  # Mostrar un mensaje flash
+        return redirect(url_for('Index'))  # Redirigir a la ruta principal
 
+# Ruta para obtener los detalles de un contacto para editarlo
 @app.route('/EditarContactos/<id>')
 def get_contact(id):
- cursor = mysql.connection.cursor()
- cursor.execute("SELECT * FROM contactos WHERE id = %s", (id))
- data = cursor.fetchall()
- return render_template('editar.html', contact = data[0])
+    cursor = mysql.connection.cursor()  # Crear un cursor para interactuar con la base de datos
+    cursor.execute("SELECT * FROM contactos WHERE id = %s", (id,))  # Ejecutar una consulta SQL para obtener un contacto por ID
+    data = cursor.fetchall()  # Obtener el resultado de la consulta
+    return render_template('editar.html', contact=data[0])  # Renderizar la plantilla editar.html con los datos obtenidos
 
-@app.route("/update/<id>", methods = ["POST"])
+# Ruta para actualizar los detalles de un contacto, método POST
+@app.route("/update/<id>", methods=["POST"])
 def update_contact(id):
- if request.method == 'POST':
-  nombre = request.form['nombre']
-  apellido = request.form['apellido']
-  telefono = request.form['telefono']
-  correo = request.form['correo']
-  cursor = mysql.connection.cursor()
-  cursor.execute("""
-                 UPDATE contactos
-                 SET nombre = %s,
-                 apellido = %s,
-                 telefono = %s,
-                 correo = %s
-                 WHERE id = %s
-                 """, (nombre,apellido,telefono,correo, id))
-  mysql.connection.commit()
-  flash('Contacto editato correctamente')
-  return redirect(url_for('Index'))
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        telefono = request.form['telefono']
+        correo = request.form['correo']
+        
+        # Crear un cursor para interactuar con la base de datos
+        cursor = mysql.connection.cursor()
+        # Ejecutar una consulta SQL para actualizar el contacto
+        cursor.execute("""
+                       UPDATE contactos
+                       SET nombre = %s,
+                           apellido = %s,
+                           telefono = %s,
+                           correo = %s
+                       WHERE id = %s
+                       """, (nombre, apellido, telefono, correo, id))
+        mysql.connection.commit()  # Confirmar los cambios en la base de datos
+        flash('Contacto editado correctamente')  # Mostrar un mensaje flash
+        return redirect(url_for('Index'))  # Redirigir a la ruta principal
 
+# Ruta para eliminar un contacto
 @app.route('/eliminarContacto/<string:id>')
 def eliminarContacto(id):
- cursor = mysql.connection.cursor()
- cursor.execute("DELETE FROM contactos WHERE id = {0}".format(id))
- mysql.connection.commit()
- flash('Contacto eliminado exitosamente')
- return redirect(url_for('Index'))
+    cursor = mysql.connection.cursor()  # Crear un cursor para interactuar con la base de datos
+    cursor.execute("DELETE FROM contactos WHERE id = {0}".format(id))  # Ejecutar una consulta SQL para eliminar un contacto por ID
+    mysql.connection.commit()  # Confirmar los cambios en la base de datos
+    flash('Contacto eliminado exitosamente')  # Mostrar un mensaje flash
+    return redirect(url_for('Index'))  # Redirigir a la ruta principal
 
+# Iniciar la aplicación Flask
 if __name__ == '__main__':
- app.run(port = 3000, debug=True)
+    app.run(port=3000, debug=True)  # Ejecutar la aplicación en el puerto 3000 con el modo de depuración activado
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,20 @@
-const btnDelete = document.querySelectorAll('.btn-delete')
+// Selecciona todos los elementos con la clase 'btn-delete'
+const btnDelete = document.querySelectorAll('.btn-delete');
 
-if (btnElimbtnDeleteinar) {
-   const btnArray =  Array.from(btnDelete);
-   btnArray.forEach((btn) => {
-    btn.addEventListener('click',(e) =>{
-        if (!confirm('Quieres borrar este registro ??')) {
-            e.preventDefault();
-        }
+// Comprueba si hay elementos seleccionados
+if (btnDelete.length > 0) {
+    // Convierte la NodeList a un array
+    const btnArray = Array.from(btnDelete);
+    
+    // Itera sobre cada botón en el array
+    btnArray.forEach((btn) => {
+        // Añade un evento 'click' a cada botón
+        btn.addEventListener('click', (e) => {
+            // Muestra una ventana de confirmación al usuario
+            if (!confirm('¿Quieres borrar este registro?')) {
+                // Si el usuario cancela, previene la acción predeterminada (la eliminación)
+                e.preventDefault();
+            }
+        });
     });
-   });
 }
